6f2dfade2d29dfd7c2155f93fa21d1dbf01d1be4
Ubuntu <ubuntu@ip-172-31-14-66.us-east-2.compute.internal>
2018-07-18 05:16:49 +0000

    *.py

---
+++ appendbytes.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python
+import sys
+import os
+
+if len(sys.argv) != 4:
+  print("USAGE addbytes.py filename byte filesizewanted")
+  quit()
+
+ifile = sys.argv[1]
+hexbyte = int(sys.argv[2])
+count = int(sys.argv[3])
+f = open(ifile, "a")
+
+f.seek(0, os.SEEK_END)
+size = f.tell()
+#print(size)
+#f.seek(size, 0)
+
+for number in range(0, (count - size)):
+  f.write(bytearray([hexbyte]))
+
